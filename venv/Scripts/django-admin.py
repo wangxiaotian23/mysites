2d2e647634c4e36817903a484b07454e0e6673cd
wangxiaotian <13576149791@163.com>
2019-10-31 19:18:43 +0800

    Initial commit

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\dell.DESKTOP-CPC1DBI\PycharmProjects\mysite\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
